a9572263bc20304f925ce5ee2677535eb848c80e
iraqez <vladimir.gumenyuk@gmail.com>
2015-06-18 13:49:08 +0300

    мой магазин

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.core import urlresolvers
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 from catalog.models import Category
 
+
+def menuSel():
+    submenu = Category.objects.all()
+    context = RequestContext(request, {'submenu': submenu})
+    return HttpResponse(template.render(submenu))

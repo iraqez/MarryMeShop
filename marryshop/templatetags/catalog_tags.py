0cad58f37d3665fc8c258738a9b919a542676b77
Iraqez <vladimir.gumenyuk@gmail.com>
2015-06-27 23:41:57 +0300

    мой инет-магазин

--- marryshop/templatetags/catalog_tags.py
+++ marryshop/templatetags/catalog_tags.py
@@ -6,13 +6,16 @@ register = template.Library()
 
 @register.inclusion_tag("menu_sel.html")
 def menu():
+    subm = []
     menu_cat = Category.MENU
-    # for i in Category.objects.filter(first_menu=menu_cat[0][0]):
+    for i in Category.objects.filter(first_menu=menu_cat[0][0]):
+        subm.append(i)
 
 
 
     return {
         "menu_cat": menu_cat,
+        "subm": subm,
     }
 
 

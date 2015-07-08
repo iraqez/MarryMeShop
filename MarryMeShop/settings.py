247e3e081f1c61b433ec809e9124aedf5a1165c9
iraqez <vladimir.gumenyuk@gmail.com>
2015-07-08 18:01:38 +0300

    мой инет-магазин

--- MarryMeShop/settings.py
+++ MarryMeShop/settings.py
@@ -96,10 +96,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'staticfiles', ),
-]
-
+# Темплейты
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',

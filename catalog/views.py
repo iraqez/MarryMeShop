3957fc57a36295b8f998e200be20d2ac5deeb8bc
iraqez <vladimir.gumenyuk@gmail.com>
2015-07-12 12:06:11 +0300

    мой инет-магазин

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, render
 from django.template import RequestContext, loader
 from django.core import urlresolvers
 from django.http import HttpResponseRedirect
@@ -8,9 +8,10 @@ from catalog.models import Category, Product
 
 
 def index(request):
-    template = loader.get_template('catalog/templates/index.html')
-    page_title = 'Marry-Me все для свадеб'
-    return render_to_response(template, locals(), context_instance=RequestContext(request))
+    return render(request, 'catalog/templates/index.html')
+    # template = loader.get_template('catalog/templates/index.html')
+    # page_title = 'Marry-Me все для свадеб'
+    # return render_to_response(template,)#, locals(), context_instance=RequestContext(request))
 
 #
 # def show_category(request, category_slug, template_name="catalog/index.html"):
